f6a7a5acfd5f28baa4bb6d2aa2d8fe5a9659d9fb
barturba <b@bartas.co>
2024-07-30 16:14:47 -0600

    chore: Refactor crawlPage function and improve error handling

--- crawl.js
+++ crawl.js
@@ -30,6 +30,7 @@ function getURLsFromHTML(html, baseURL) {
 }
 
 async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+  // await new Promise((r) => setTimeout(r, 1000));
   // 1. make sure currentURL is the same domain as the baseURL. If it's not, just
   // return the current pages. We don't want to crawl the entire internet,
   // just the domain in question.
@@ -69,50 +70,61 @@ async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
   // 2. Get a normalized version of the currentURL.
 
   const normalizedURL = normalizeURL(currentURL);
-  console.log(`normalizedURL: ${normalizedURL}`);
+  // console.log(`normalizedURL: ${normalizedURL}`);
 
   //
   // 3. If the pages object already has an entry for the normalized version of
   // the current URL, just increment the count and return the current pages.
-  if (containsObject(pages, normalizedURL)) {
-    pages[normalizedURL] += 1;
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++;
     return pages;
   }
-
+  // console.log(`pages: doesn't have ${normalizedURL}`);
   //
   // 4. Otherwise, add an entry to the pages object for the normalized version
   // of the current URL, and set the count to 1.
-
   pages[normalizedURL] = 1;
-  console.log(`pages: ${JSON.stringify(pages)}`);
-  var text = "";
-  try {
-    text = await fetchURL(currentURL);
-  } catch (error) {
-    console.error(error.message);
-  }
-  // console.log(`text: ${JSON.stringify(text)}`);
+  // console.log(`pages: ${JSON.stringify(pages)}`);
+
   //
   // 5. If you've gotten here, break out the logic for fetching the current URL
   // and parsing the HTML into its own function. This will keep the crawl
   // functionality readable and manageable. Call the new function inside
   // crawlPage.
-  var links = "";
+
+  var text = "";
   try {
-    links = await getURLsFromHTML(text, baseURL);
+    text = await fetchURL(currentURL);
   } catch (error) {
     console.error(error.message);
+    exit();
   }
-  console.log(`links: ${JSON.stringify(links, 0, 2)}`);
 
   //
   // 6. Assuming all went well with the fetch request in the new function, get
   // all the urls from the response body html.
   //
+
+  // console.log(`fetching URLs`);
+  var links = "";
+  try {
+    links = await getURLsFromHTML(text, baseURL);
+  } catch (error) {
+    console.error(error.message);
+    exit();
+  }
+  // console.log(`links: ${JSON.stringify(links, 0, 2)}`);
+
   // 7. Recursively crawl each url you found on the page and update the pages to
   // keep and aggregate count.
+
+  for (const url of links) {
+    pages = await crawlPage(baseURL, url, pages);
+  }
   //
   // 8. Finally, return the updated pages object.
+
+  return pages;
 
   // try {
   //   const response = await fetch(currentURL);
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { argv, exit } from "node:process";
 import { crawlPage } from "./crawl.js";
 
-function main() {
+async function main() {
   if (argv.length < 3) {
     console.log("number of arguments less than 1");
     exit();
@@ -13,7 +13,8 @@ function main() {
   const baseURL = argv[2];
   console.log(`starting at base URL: ${baseURL}`);
   try {
-    crawlPage(baseURL);
+    var pages = await crawlPage(baseURL);
+    console.log(JSON.stringify(pages, 0, 2));
   } catch (error) {
     console.error(error.message);
     exit();

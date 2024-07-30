e98cfd0e45d77b32953eda135e309920fbcd6c89
barturba <b@bartas.co>
2024-07-30 11:16:13 -0600

    feat: Add command line argument handling to main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import { argv, exit } from "node:process";
+
+function main() {
+  if (argv.length < 3) {
+    console.log("number of arguments less than 1");
+    exit();
+  } else if (argv.length > 3) {
+    console.log("number of arguments greater than 1");
+    exit();
+  }
+  const baseURL = argv[2];
+  console.log(`starting at base URL: ${baseURL}`);
+}
+
+main();

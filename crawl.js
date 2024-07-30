96e445288b91d2f14d1ff87ac700d67e125d10ba
barturba <b@bartas.co>
2024-07-30 16:40:48 -0600

    Refactor crawlPage function and improve error handling

--- crawl.js
+++ crawl.js
@@ -83,15 +83,4 @@ async function fetchURL(url) {
   }
 }
 
-function containsObject(obj, list) {
-  var i;
-  for (i = 0; i < list.length; i++) {
-    if (list[i] === obj) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
 export { normalizeURL, getURLsFromHTML, crawlPage };
